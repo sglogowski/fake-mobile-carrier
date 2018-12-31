713785ba4e68715c17bd788809f403262569f008
sglogowski <s.glogowski@outlook.com>
2018-12-31 18:46:21 +0100

    finalize app.

---
+++ src/FakeMobileCarrier.CallCenter.WebApp/App_Start/AppModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using FakeMobileCarrier.Model;
+
+namespace FakeMobileCarrier.CallCenter.WebApp.App_Start
+{
+    public class AppModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .RegisterType<FakeMobileCarrierDbContext>()
+                .AsSelf()
+                .As<IFakeMobileCarrierDbContext>()
+                .InstancePerRequest();
+        }
+    }
+}
